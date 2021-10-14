ac30eddfc4eb1700b58edce0564136a06b100351
parthkanani93 <parthkanani8490@gmail.com>
2021-10-15 00:27:04 +0530

    added jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'react-native',
+  testRegex: '(/__tests__/.*|/src/.*\\.(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'ios.ts',
+    'ios.tsx',
+    'android.ts',
+    'android.tsx',
+  ],
+};
